391b7a8120581e265a10ff9e1310ba7eb6d2100d
oleg.zeller <oleg.zeller@RV24-LT-043>
2024-11-20 08:26:38 +0100

    Neues Messagefenster WIP

---
+++ EingangsScan/EingangsScanMessageBox.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EingangsScan
+{
+    /// <summary>
+    /// Interaktionslogik für EingangsScanMessageBox.xaml
+    /// </summary>
+    public partial class EingangsScanMessageBox : Window
+    {
+        private string Message { get; set; }
+
+        public EingangsScanMessageBox(string message)
+        {
+            InitializeComponent();
+            Message = message;
+            DataContext = this;
+        }
+
+
+        private void Button_Storno(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Close(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Reaktivieren(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
